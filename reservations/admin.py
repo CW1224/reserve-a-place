fc04a2886668964676b5a0c304238d9b8eb4125d
CW1224 <xixiwang1224@gmail.com>
2023-04-22 22:01:05 +0000

    Added in booking model, approval request and created superuser

--- reservations/admin.py
+++ reservations/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Booking
 
-# Register your models here.
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_filter = ('status', 'created_date')
+    actions = ['approve_booking']
+    list_display = ['user', 'number_guests', 'high_chair','booking_date', 'booking_time', 'status']
+
+    def approve_booking(self, request, queryset):
+        queryset.update(approved=True)
--- reservations/models.py
+++ reservations/models.py
@@ -1,3 +1,22 @@
+import uuid
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+# Import Django authentication user system
+STATUS = ((0, 'Unapproved'), (1, 'Approved'))
+
+class Booking(models.Model):
+    booking_id = models.UUIDField(primary_key=True, unique=True, default=uuid.uuid4, editable=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='reservations')
+    created_date = models.DateTimeField(auto_now=True)
+    booking_date = models.DateField(auto_now=False)
+    booking_time = models.TimeField(auto_now=False)
+    number_guests = models.IntegerField()
+    booking_comments = models.TextField(max_length=500, blank=True)
+    high_chair = models.BooleanField(null=False, blank=False, default=False)
+    status = models.IntegerField(choices=STATUS, default=0)
+
+    class Meta:
+        ordering = ['-booking_date']
+

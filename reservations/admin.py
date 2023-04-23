86ab1ea232f83ed6a8d5e6a399eb7aa3b2c0998c
CW1224 <xixiwang1224@gmail.com>
2023-04-23 21:37:32 +0000

    Added in the navigation template from codestar

--- reservations/admin.py
+++ reservations/admin.py
@@ -6,6 +6,8 @@ class BookingAdmin(admin.ModelAdmin):
     list_filter = ('status', 'created_date')
     actions = ['approve_booking']
     list_display = ['user', 'number_guests', 'high_chair','booking_date', 'booking_time', 'status']
+    search_fields = ['user', 'created_date', 'status', 'booking_date']
+    actions = ['approve_booking']
 
     def approve_booking(self, request, queryset):
         queryset.update(approved=True)

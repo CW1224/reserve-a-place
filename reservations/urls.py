24d1b5e40423dfc9c3759abc5d78410a67af9425
CW1224 <xixiwang1224@gmail.com>
2023-05-09 19:51:45 +0000

    Added in navigation bar and modified title

--- reservations/urls.py
+++ reservations/urls.py
@@ -2,6 +2,7 @@ from . import views
 from django.urls import path, include
 
 urlpatterns = [
+    path('', views.BookingList.as_view(), name='home'),
     path('reservation_detail/<str:booking_id>/', views.BookingDetail.as_view(), name='reservation_detail.urls'), 
     path('', views.BookingList.as_view(), name='reservations.urls'),
 ]
